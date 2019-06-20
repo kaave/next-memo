c8fced27362e7bd0ad04c8841bb7238bfd4de4e2
kaave <junkjunctions@gmail.com>
2019-06-20 10:39:10 +0900

    add: ts type checker

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const withTypescript = require('@zeit/next-typescript');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const withSass = require('@zeit/next-sass');
 
-module.exports = withTypescript(
-  withSass({
-    webpack(config) {
-      return config;
-    },
-  }),
-);
+function webpack(config, options) {
+  if (options.isServer) {
+    config.plugins.push(new ForkTsCheckerWebpackPlugin());
+  }
+
+  return config;
+}
+
+module.exports = withTypescript(withSass({ webpack }));
