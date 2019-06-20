8f67d7b4c8468f4ae9b9db935697d7e3d6417274
kaave <junkjunctions@gmail.com>
2019-06-20 11:47:58 +0900

    add PostCSS plugins

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+const plugins = {
+  'postcss-custom-properties': {},
+  'postcss-color-hex-alpha': {},
+  'postcss-calc': {},
+  'postcss-flexbugs-fixes': {},
+  'postcss-url': {},
+  autoprefixer: { grid: true },
+};
+
+module.exports = { plugins };
