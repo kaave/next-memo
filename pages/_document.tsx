30109707e32ec01d08a9ee4f862310eb7a427612
kaave <abe@framelunch.jp>
2019-06-21 15:58:27 +0900

    add: polyfill

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript, NextDocumentContext } from 'next/document';
 
+import * as configs from '../utils/configs';
+
 class ModifiedDocument extends Document {
   static async getInitialProps(ctx: NextDocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -11,7 +13,9 @@ class ModifiedDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script src={`https://polyfill.io/v3/polyfill.min.js?features=${configs.polyfills}&flags=gated`} defer />
+        </Head>
         <body>
           <Main />
           <NextScript />
---
+++ utils/configs.ts
@@ -0,0 +1,1 @@
+export const polyfills = ['default', 'es2015', 'es2016', 'es2017', 'IntersectionObserver'].join('%2C');
