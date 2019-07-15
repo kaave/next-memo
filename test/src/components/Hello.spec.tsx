605ece0be46cabfd4f5470896299f6d730871621
kaave <junkjunctions@gmail.com>
2019-07-15 16:32:56 +0900

    add: test

---
+++ test/src/components/Hello.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Hello from '~/components/Hello';
+
+describe('<Hello />', () => {
+  it('props.nameなしだとHello! Jane Doe', () => {
+    const renderer = shallow(<Hello />);
+    expect(renderer.text().includes('Jane Doe')).toBeTruthy();
+  });
+
+  it('props.nameありだとHello! <わたした名前>', () => {
+    const name = 'Novak Đoković';
+    const renderer = shallow(<Hello {...{ name }} />);
+    expect(renderer.text().includes(name)).toBeTruthy();
+  });
+});
