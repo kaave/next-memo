f4b0bbe0117ddc87719d04e121f4892b980b4ebb
kaave <junkjunctions@gmail.com>
2019-07-26 16:41:33 +0900

    add: sort-package-json

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,5 @@ module.exports = {
   '*.{js,ts,tsx}': ['eslint --fix', 'git add'],
   '*.scss': ['stylelint --fix', 'git add'],
   '*.json': ['prettier --write', 'git add'],
+  'package.json': ['sort-package-json', 'git add'],
 };
